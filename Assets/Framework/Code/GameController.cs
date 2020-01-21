2121ad574aa10078aed44d147522be0ddfcaa7e9
Jeremy <jeremy.m.legere@gmail.com>
2020-01-20 21:24:44 -0400

    Add TODOs for GameController

--- Assets/Framework/Code/GameController.cs
+++ Assets/Framework/Code/GameController.cs
@@ -4,73 +4,77 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject player;
 
     public GameController()
     {
         //Constructor
     }
-    
+
     //Up Input
     public void OnUpInputDown()
     {
-
+        //TODO: Occurs once when UP input is pressed
     }
 
     public void OnUpInputHold()
     {
-
+        //TODO: Occurs every update/fixedUpdate when UP input is held down
     }
 
     public void OnUpInputUp()
     {
-
+        //TODO: Occurs once when UP input is released
     }
 
+    //---------------------------------------------------------------------------
     //Down Input
     public void OnDownInputDown()
     {
-
+        //TODO: Occurs once when DOWN input is pressed
     }
 
     public void OnDownInputHold()
     {
-
+        //TODO: Occurs every update/fixedUpdate when DOWN input is held down
     }
 
     public void OnDownInputUp()
     {
-
+        //TODO: Occurs once when DOWN input is released
     }
 
+    //---------------------------------------------------------------------------
     //Left Input
     public void OnLeftInputDown()
     {
-
+        //TODO: Occurs once when LEFT input is pressed
     }
 
     public void OnLeftInputHold()
     {
-
+        //TODO: Occurs every update/fixedUpdate when LEFT input is held down
     }
 
     public void OnLeftInputUp()
     {
-
+        //TODO: Occurs once when LEFT input is released
     }
 
+    //---------------------------------------------------------------------------
     //Right Input
     public void OnRightInputDown()
     {
-
+        //TODO: Occurs once when RIGHT input is pressed
     }
 
     public void OnRightInputHold()
     {
-
+        //TODO: Occurs every update/fixedUpdate when RIGHT input is held down
     }
 
     public void OnRightInputUp()
     {
-
+        //TODO: Occurs once when RIGHT input is released
     }
 }
